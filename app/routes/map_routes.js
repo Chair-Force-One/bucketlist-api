6afbf5d3dff5fc41ec750b25399de487324948dc
ckempema <calderkempema@gmail.com>
2018-11-07 15:26:30 -0500

    Updated map to actually work
    Calder/Josh
    
    Stored key in secure environment to return to reduce risk

--- app/routes/map_routes.js
+++ app/routes/map_routes.js
@@ -1,34 +1,16 @@
-// Express docs: http://expressjs.com/en/api.html
+'use strict'
+
 const express = require('express')
 
-// pull in Mongoose model for adventures
-// const Adventure = require('../models/adventure')
-
-// we'll use this to intercept any errors that get thrown and send them
-// back to the client with the appropriate status code
-const handle = require('../../lib/error_handler')
-
-// this is a collection of methods that help us detect situations when we need
-// to throw a custom error
-// const customErrors = require('../../lib/custom_errors')
-
-const loadGoogleMapsApi = require('load-google-maps-api')
-
-// we'll use this function to send 404 when non-existant document is requested
-// const handle404 = customErrors.handle404
-
-// instantiate a router (mini app that only handles routes)
 const router = express.Router()
 
 const dotenv = require('dotenv')
 dotenv.config()
-const initOptions = process.env.GOOGLE_MAP_API_KEY
+
 // INDEX
 // GET /adventures
 router.get('/maps', (req, res) => {
-  loadGoogleMapsApi(initOptions)
-    .then(map => res.status(200).json({ map: map }))
-    .catch(err => handle(err, res))
+  res.status(200).json({key: process.env.GOOGLE_MAP_API_KEY})
 })
 
 module.exports = router
